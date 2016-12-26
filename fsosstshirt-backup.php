d479c87c786b8a7b1e72aec908f92631a705eed7
Oleg Mitrakhovich <olegmit9@hotmail.com>
2016-12-26 18:18:11 -0500

    Update fsosstshirt-backup.php

--- fsosstshirt-backup.php
+++ fsosstshirt-backup.php
@@ -65,7 +65,7 @@ if ($_POST) {
 	}
 
 	if ($datavalid){
-	$link = mysqli_connect("db-mysql.zenit", "int322_163d06", "W6dvjw12345&*", "int322_163d06")
+	$link = mysqli_connect("db-mysql.zenit", "int322_163d06", "PASSWORD", "int322_163d06")
 				or die('Could not connect: ' . mysqli_error($link));
 	$sql_query = 'INSERT INTO orders2 set firstName="' . $firstname . '", lastName="' . $lastname . '", sex="' . $sex . '", mltp="' . $yesno . '", 
 	number="' . $ordernumber . '", size= "' . $size . '"';
